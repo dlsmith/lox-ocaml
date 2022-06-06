7ff3bc4d96dfd1d2ff7a2f36d0cb576367da0e78
David Smith <david.lawrence.smith@gmail.com>
2022-06-06 17:13:12 -0400

    Wire together scanner and parser for basic interpreter functionality.

--- lib/interpreter/interpreter.ml
+++ lib/interpreter/interpreter.ml
@@ -1,5 +1,26 @@
+let (let*) = Result.bind
+
 let run source =
-    print_endline source; Ok ()
+    let token_results =
+        source
+        |> Scanner.scan_tokens
+        (* TODO(dlsmith): Consume `Seq` directly. *)
+        |> List.of_seq in
+    let* tokens =
+        match List.map Result.get_ok token_results with
+        (* TODO(dlsmith): Surface all errors. *)
+        | exception Invalid_argument message -> Error message
+        | tokens -> Ok tokens
+    in
+    let* expr, remaining_tokens =
+        Parser.parse_expression tokens
+        (* TODO(dlsmith): Surface all errors. *)
+        |> Result.map_error (fun (message, _) -> message)
+    in
+    match remaining_tokens with
+    | [ { token_type=Token.EOF; _} ] ->
+        print_endline (Parser.to_sexp expr); Ok ()
+    | _ -> Error "Expected a single expression"
 
 let report line where message =
     Printf.printf "[line %i] Error %s: %s" line where message
